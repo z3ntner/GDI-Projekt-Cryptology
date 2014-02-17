c58869cdba2f7ede40b97713f0949456c58edb29
Federzoni <simon.physik@gmx.de>
2014-02-17 18:18:11 +0100

    Simon testet GitHub also der eine, nicht der andere

---
+++ GDI-Projekt-Template/src/basisaufgaben/TestClass.java
@@ -0,0 +1,57 @@
+package basisaufgaben;
+
+import java.util.Iterator;
+
+import de.tudarmstadt.gdi1.project.alphabet.Alphabet;
+
+public class TestClass implements Alphabet {
+
+	@Override
+	public Iterator<Character> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getIndex(char chr) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public char getChar(int index) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean contains(char chr) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean allows(String word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String normalize(String input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public char[] asCharArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
