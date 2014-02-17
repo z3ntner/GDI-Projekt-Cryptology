604a8aff60e42203bef380a2f76fcb2af96d67eb
Fabian Zentner <fabian.zentner@t-online.de>
2014-02-17 18:12:49 +0100

    test123

--- GDI-Projekt-Template/src/basisaufgaben/basicalphabet.java
+++ GDI-Projekt-Template/src/basisaufgaben/basicalphabet.java
@@ -1,5 +1,46 @@
 package basisaufgaben;
 
-public class simplealphabet {
+import de.tudarmstadt.gdi1.project.alphabet.Alphabet;
+import java.util.*;
+
+public class basicalphabet implements Alphabet {
+	
+	private char[] characters = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l'};
+	
+	@Override
+	public int getIndex(char chr){
+		return characters.binarySearch(chr);
+	}
+	
+	@Override
+	public char getChar(int index){
+		return characters[index];
+		
+	}
+	
+	@Override
+	public int size(){
+		return characters.length;
+		
+	}
+	
+	@Override
+	public boolean contains(char chr){
+		
+	}
+	
+	@Override
+	public boolean allows(String word){
+		
+	}
+	@Override
+	public String normalize(String input){
+		
+	}
+	
+	@Override
+	public char[] asCharArray(){
+		
+	}
 
 }
